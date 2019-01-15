6fe2490caa32ef7d7d2013ee1aac5f9dfd34b359
James Webb <jrwebb1@sheffield.ac.uk>
2019-01-15 21:02:33 +0000

    Added board border

--- p5js/empty-example/sketch.js
+++ p5js/empty-example/sketch.js
@@ -5,7 +5,7 @@ const TOTAL_LENGTH = TILE_SIDE_LENGTH * GRID_SIDE_LENGTH;
 var isGridDrawn;
 
 function setup() {
-    createCanvas(TOTAL_LENGTH, TOTAL_LENGTH);
+    createCanvas(TOTAL_LENGTH + 1, TOTAL_LENGTH + 1);
     drawGrid();
 
 }
@@ -15,14 +15,15 @@ function draw() {
 }
 
 function drawGrid() {
-    // fill(125)
-    // rect(0, 0, TOTAL_LENGTH + 10, TOTAL_LENGTH + 10)
+    const speed = 15;
+    fill(0)
+    rect(0, 0, TOTAL_LENGTH + 1, TOTAL_LENGTH + 1)
     for (let y = 0; y < GRID_SIDE_LENGTH; y++) {
         for (let x = 0; x < GRID_SIDE_LENGTH; x++) {
             setTimeout(() => {
                 console.log(x, y, GRID_SIDE_LENGTH)
                 drawTile(x, y, ((x + (y % 2 == 0 ? 1 : 0)) % 2) * 255);
-            }, ((y * GRID_SIDE_LENGTH) + x) * 15)
+            }, ((y * GRID_SIDE_LENGTH) + x) * speed)
         }
     }
 }
