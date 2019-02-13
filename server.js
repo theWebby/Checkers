e60c12a3449f88c50237fb78abe1af9edf74e651
James Webb <jrwebb1@sheffield.ac.uk>
2019-02-13 19:00:32 +0000

    added try catch

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ var app = express();
 // Set up the server
 // process.env.PORT is related to deploying on heroku
 // app.use(serveStatic('public', {'index': 'index.html'}));
-app.use(express.static('public'))
-var server = app.listen(80);
+
+try{
+    app.use(express.static('public'))
+    var server = app.listen(80);
+}
+catch(e){
+    console.log(e);
+}
 
 // // This call back just tells us that the server has started
 // function listenCallback() {
